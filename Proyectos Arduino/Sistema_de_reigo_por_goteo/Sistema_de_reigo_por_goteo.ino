641eb582003ea676870fc115952f821770c9c0ae
Emanuel <107447125+ender346@users.noreply.github.com>
2023-02-02 15:44:57 -0600

    Sistema de Riego

--- Proyectos Arduino/Sistema_de_reigo_por_goteo/Sistema_de_reigo_por_goteo.ino
+++ Proyectos Arduino/Sistema_de_reigo_por_goteo/Sistema_de_reigo_por_goteo.ino
@@ -1,61 +1,71 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
+#include <DHT_U.h>
 
-//pins
-const int bombaAgua = 0;
-const int sensor = 9;
-const int boton = 10;
+//pines
 
-//objects
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-DHT dht(sensor, DHT11);
+int bombaAgua = 0;
+int sensor = 9;
+int boton = 10;
+
+//variables
+int temp;
+int sensorTemp;
+int eatadoBoton;
+
+//objetos
+LiquidCrystal lcd(7,6,5,4,3,2);
+DHT dht(sensorTemp, DHT11);
 
 void setup() {
   lcd.begin(16, 2);
 
   pinMode(bombaAgua, OUTPUT);
+
+  dht.begin();
+
   pinMode(boton, INPUT);
   
-  dht.begin();
 }
 
 void loop() {
   int temp = dht.readTemperature();
   int hum = dht.readHumidity();
-  int estadoBoton = digitalRead(boton);
+  int estadoBoton=digitalRead(boton);
 
-  lcd.setCursor(0, 0);
-  lcd.print("Temperature: " + String(temp) + " °C");
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity: " + String(hum) + " %");
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura" + String(temp) +" °C");
+  lcd.setCursor(0,1);
+  lcd.print("Humedad: "+ String(hum) +" %    ");
 
-  if (temp >= 25 && hum <= 20) {
+  if(temp>=25&&hum<=20){
+     digitalWrite(bombaAgua, LOW);
+
+     lcd.setCursor(0,0);
+     lcd.print("*****Regando*****");
+     lcd.setCursor(0,1);
+     lcd.print("*****************");
+     delay(500);
+     
+  } else if(estadoBoton==HIGH){
     digitalWrite(bombaAgua, LOW);
 
-    lcd.setCursor(0, 0);
-    lcd.print("Watering...");
-    lcd.setCursor(0, 1);
-    lcd.print("           ");
-    delay(500);
-  } 
-  else if (estadoBoton == HIGH) {
-    digitalWrite(bombaAgua, LOW);
-
-    lcd.setCursor(0, 0);
-    lcd.print("Watering...");
-    lcd.setCursor(0, 1);
-    lcd.print("           ");
-    delay(1000);
-  } 
-  else {
+     lcd.setCursor(0,0);
+     lcd.print("*****Regando*****");
+     lcd.setCursor(0,1);
+     lcd.print("*****************");
+     delay(1000);
+      
+  } else {
     digitalWrite(bombaAgua, HIGH);
   }
 
-  if (temp == 0 && hum == 0) {
-    lcd.setCursor(0, 0);
-    lcd.print("Error: No sensor");
-    lcd.setCursor(0, 1);
-    lcd.print("       connected");
+  if(temp==0&&hum==0){
+    lcd.setCursor(0,0);
+    lcd.print("*****ERROR*****");
+    lcd.setCursor(0,1);
+    lcd.print("***NO SENSOR***");
     delay(1000);
+    
   }
 }
